30b23f5118c6943239263341b3c27d0f014b37bc
Aaron Lin <Aaron.Lin@ehealth-china.com>
2016-01-06 16:58:38 +0800

    add flyway

---
+++ src/main/resources/db-init/data/V3_001_01__JOB_DEV.sql
@@ -0,0 +1,7 @@
+INSERT INTO JOB_DEV (JOB_ID, JOB_NAME, JOB_GROUP, TARGET_OBJECT, TARGET_METHOD, JOB_STATUS, CORN_EXPRESSION)
+VALUES
+  ('6eb2d68b-1654-4214-bcc7-f1b3fe9b736e', 'jobOne', 'ehi', 'com.ehi.tool.job.JobOne', 'jobOne', '0', '0/5 * * * * ?');
+
+INSERT INTO JOB_DEV (JOB_ID, JOB_NAME, JOB_GROUP, TARGET_OBJECT, TARGET_METHOD, JOB_STATUS, CORN_EXPRESSION)
+VALUES
+  ('f647983f-f8d9-47a1-9ec8-fe60048bbfb0', 'jobTwo', 'ehi', 'com.ehi.tool.job.JobTwo', 'jobTwo', '0', '0/5 * * * * ?');
